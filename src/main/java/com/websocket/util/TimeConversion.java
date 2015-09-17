62bae76d03a32da4a5d9ff3c0fad3fd471283043
tiwarijatin <jatintiwari11@gmail.com>
2015-09-17 14:47:51 +0530

    Time set to IST

--- src/main/java/com/websocket/util/TimeConversion.java
+++ src/main/java/com/websocket/util/TimeConversion.java
@@ -1,12 +1,32 @@
 package com.websocket.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class TimeConversion {
+	
+	private static Long getTime(){
+		Calendar calendar = Calendar.getInstance();
+	    TimeZone fromTimeZone = calendar.getTimeZone();
+	    TimeZone toTimeZone = TimeZone.getTimeZone("IST");
+
+	    calendar.setTimeZone(fromTimeZone);
+	    calendar.add(Calendar.MILLISECOND, fromTimeZone.getRawOffset() * -1);
+	    if (fromTimeZone.inDaylightTime(calendar.getTime())) {
+	        calendar.add(Calendar.MILLISECOND, calendar.getTimeZone().getDSTSavings() * -1);
+	    }
+
+	    calendar.add(Calendar.MILLISECOND, toTimeZone.getRawOffset());
+	    if (toTimeZone.inDaylightTime(calendar.getTime())) {
+	        calendar.add(Calendar.MILLISECOND, toTimeZone.getDSTSavings());
+	    }
+	    return calendar.getTimeInMillis();
+	}
 
 	public static String getCurrentDateAndTime(){
-		Date date = new Date(System.currentTimeMillis());
+		Date date = new Date(getTime());
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm a");
 		return sdf.format(date);
 	}
@@ -21,13 +41,13 @@ public class TimeConversion {
 		return sdf.format(date);
 	}
 	public static String getCurrentDate(){
-		Date date = new Date(System.currentTimeMillis());
+		Date date = new Date(getTime());
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		return sdf.format(date);
 	}
 	
 	public static String getCurrentTime(){
-		Date date = new Date(System.currentTimeMillis());
+		Date date = new Date(getTime());
 		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
 		return sdf.format(date);
 	}
